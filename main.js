e7552a34c862009d99b49bff95a6264b783c4efc
theGeekyLad <rahul.pillai03@gmail.com>
2021-03-16 00:05:30 +0530

    Supports G413

---
+++ main.js
@@ -0,0 +1,96 @@
+const { app, BrowserWindow } = require('electron')
+const { ipcMain } = require('electron')
+const { exec } = require('child_process');
+
+let process = null
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 600,
+        height: 250,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
+// runtime
+ipcMain.on('pulse-led', (event, val) => {
+    console.log('Pulse...', val)
+    exec('g413-led -fx breathing all ff0000 ' + val, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+})
+
+ipcMain.on('on-led', (event) => {
+    console.log('Off...')
+    exec('g413-led -a ff0000', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+})
+
+ipcMain.on('off-led', (event) => {
+    console.log('Off...')
+    exec('g413-led -a 000000', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+})
+
+ipcMain.on('change-led', (event, val) => {
+    console.log('Change...', val)
+    exec('g413-led -a ' + val + '0000', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+})
+
+ipcMain.on('hook-notifications', (event) => {
+    console.log('Hooking...')
+    process = exec('python3 /home/thegeekylad/Projects/g413-dbus-led/main.py', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+})
+
+ipcMain.on('unhook-notifications', (event) => {
+    console.log('Unhooking...', process.kill());
+})
